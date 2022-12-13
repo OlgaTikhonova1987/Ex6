2fac867cb1ffa1de3d78c22481fa90797ede77d6
OlgaTikhonova1987 <119539216+OlgaTikhonova1987@users.noreply.github.com>
2022-12-13 20:01:51 +0300

    Ex62

--- src/test/java/Ex6.java
+++ src/test/java/Ex6.java
@@ -1,2 +1,19 @@
-package PACKAGE_NAME;public class Ex6 {
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Test;
+
+public class Ex6 {
+    @Test
+    public void Test6() {
+        Response response = RestAssured
+                .given()
+                .redirects()
+                .follow(false)
+                .when()
+                .get("https://playground.learnqa.ru/api/long_redirect")
+                .andReturn();
+                response.prettyPrint();
+        String location = response.getHeader("Location");
+        System.out.println(location);
+    }
 }
